dd0a71fdc385e000d62be3d99384ce200fb2bd48
Sookyung Chang <sooensible@gmail.com>
2026-05-11 13:59:47 +0000

    Feat: add min_cluster_size value, add reduce_outliers to avoid losing big data

--- src/analysis/topic_modeling.py
+++ src/analysis/topic_modeling.py
@@ -1,21 +1,31 @@
 # src/analysis/topic_modeling.py
-
+import hdbscan
 from bertopic import BERTopic
 import pandas as pd
 
-def get_topics(df: pd.DataFrame, sentiment_label: int):
+def get_topics(df: pd.DataFrame, sentiment_label: int, min_cluster_size=10):
     df_sentiment = df[df['sentiment_label'] == sentiment_label].copy()
     # ↑ add .copy() to avoid SettingWithCopyWarning
 
     # Guard: need minimum comments
     if len(df_sentiment) < 10:
         return df_sentiment, None
-
-    topic_model = BERTopic(language="english")
+    
+    hdbscan_model = hdbscan.HDBSCAN(min_cluster_size=min_cluster_size, metric="euclidean", prediction_data=True)
+    topic_model = BERTopic(
+                         # language="english",
+                           hdbscan_model=hdbscan_model,
+                           )
     topics, probs = topic_model.fit_transform(df_sentiment["text"].tolist())
     # ↑ add .tolist() — BERTopic works better with lists than pandas Series
 
-    df_sentiment['topic'] = topics
+    new_topics = topic_model.reduce_outliers(df_sentiment["text"].tolist(), topics, strategy="c-tf-idf")
+
+    topic_model.update_topics(df_sentiment["text"].tolist(), topics=new_topics)
+
+
+    # df_sentiment['topic'] = topics
+    df_sentiment['topic'] = new_topics
     df_sentiment['topic_probability'] = probs
 
     return df_sentiment, topic_model
