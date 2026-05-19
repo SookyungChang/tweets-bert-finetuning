fc897187f48e82aa2e7fb169bf4adbb88b64c393
sweetguma <sooensible@gmail.com>
2026-05-19 22:28:21 +0200

    Fix: avoid everytime downloads and share hf embeddings with ChromaDB

--- src/analysis/topic_modeling.py
+++ src/analysis/topic_modeling.py
@@ -1,12 +1,23 @@
 # src/analysis/topic_modeling.py
 import hdbscan
 from bertopic import BERTopic
+from bertopic.backend import BaseEmbedder
 import pandas as pd
+import numpy as np
+
+
+class BERTopicLangchainEmbedder(BaseEmbedder):
+    def __init__(self, lanchain_model):
+        self.model = lanchain_model
+
+    def embed(self, documents, verbose=False):
+        return np.array(self.model.embed_documents(documents))
 
 
 def get_topics(
     df: pd.DataFrame,
     sentiment_label: int,
+    embedding_model,
     min_cluster_size: int = 10,  # ← add with default value
     hdbscan_model=None,
 ):
@@ -21,9 +32,13 @@ def get_topics(
         hdbscan_model = hdbscan.HDBSCAN(
             min_cluster_size=min_cluster_size, metric="euclidean", prediction_data=True
         )
+
+    bertopic_embedding = BERTopicLangchainEmbedder(embedding_model)
+
     topic_model = BERTopic(
-        # language="english",
+        embedding_model=bertopic_embedding,
         hdbscan_model=hdbscan_model,
+        # calculate_probabilities=False,
     )
     topics, probs = topic_model.fit_transform(df_sentiment["text"].tolist())
     # ↑ add .tolist() — BERTopic works better with lists than pandas Series
