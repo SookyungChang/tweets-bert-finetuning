0a1952153f3ae9a796c063a1c53302970f4c28dd
sweetguma <sooensible@gmail.com>
2026-06-01 08:56:58 +0200

    Config: add repo_id info for fine-tuned model

--- src/config_bert.py
+++ src/config_bert.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from pathlib import Path
 
 
@@ -6,22 +6,11 @@ from pathlib import Path
 class ModelConfig:
     model_name: str = "distilbert-base-uncased-finetuned-sst-2-english"
     seed: int = 42
-    version: str = "0.1.1"
+    version: str = "0.2.1"
     num_epochs: int = 1
     f1_avg: str = "macro"
     dropout: float = 0.1
+    fine_tuned_model: str = field(init=False)
+    def __post_init__(self):
+        self.fine_tuned_model = f"sweetguma/bert-sentiment-model-v{self.version}"
 
-
-# @dataclass
-# class SearchSpace:
-#     min_df: tuple = (1, 100)
-#     max_features: tuple = (5000, 30000)
-#     C: tuple = (1e-3, 50.0)
-
-
-# @dataclass
-# class ModelConfig:
-#     seed: int = 42
-#     max_iter: int = 2000
-#     version: str = "0.1.0"
-#     f1_avg: str = "macro"
