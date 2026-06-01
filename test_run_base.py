7446342d60167fb06b742ada6a1544466df9cc46
sweetguma <sooensible@gmail.com>
2026-06-01 09:00:31 +0200

    chore:  train function does not require input anymore

--- test_run_base.py
+++ test_run_base.py
@@ -1,15 +1,10 @@
 import os
 from src.training.train_baseline import train
-from src.data.preprocess import build_dataset
 from src.inference.predictor_base import predict
 from src.config import PathConfig
 from src.inference import predictor_base
 
-
-def test_train():
-    paths = PathConfig()
-    data_path = paths.TWEETS_PATH / "tweets640k.parquet"
-    train(data_path)
+from src.data.build_datasets import build_dataset_db
 
 
 def test_inf():
@@ -22,5 +17,5 @@ def test_inf():
 
 
 if __name__ == "__main__":
-    test_train()
+    train()
     # test_inf()
