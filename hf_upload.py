9bb6f61da222c8d95eaa71af23d18d143e1cd6c9
sweetguma <sooensible@gmail.com>
2026-06-01 08:51:36 +0200

    Fix: create a repo before uploading & add version info

--- hf_upload.py
+++ hf_upload.py
@@ -1,17 +1,21 @@
-from huggingface_hub import upload_folder
+from huggingface_hub import upload_folder, create_repo
 from src.config import PathConfig
-
+from src.config_bert import ModelConfig
+paths = PathConfig()
+models = ModelConfig()
 
 def upload(folder_name):
-    paths = PathConfig()
     folder_path = paths.SAVED_MODELS_PATH / folder_name
+    repo_id=f"sweetguma/bert-sentiment-model-v{models.version}"
+    create_repo(repo_id=repo_id, repo_type="model", exist_ok=True)
+
     upload_folder(
         folder_path=folder_path,
-        repo_id="sweetguma/bert-sentiment-model",
+        repo_id=repo_id,
         repo_type="model",
     )
 
 
 if __name__ == "__main__":
-    folder_name = "bert-0.1.0/checkpoint-20000"
+    folder_name = f"bert-{models.version}/checkpoint-20000"
     upload(folder_name)
